3890988bb5eb8401c0970929477f1974e3555181
Tooga Garanja Magar <tugamagar77@gmail.com>
2023-09-24 14:48:58 -0400

    product create endpoint with validations for field

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ server.listen(PORT, HOST, function () {
   console.log('**** Resources: ****');
   console.log('********************');
   console.log('/products');
+  console.log('/products/:id');
 });
 
 server.use(restify.plugins.fullResponse());
@@ -27,5 +28,28 @@ server.get('/products', function (req, res, next) {
   productsSave.find({}, function (error, products) {
     if (error) return next(new errors.InternalServerError(JSON.stringify(error.errors)));
     res.send(products);
+  });
+});
+
+// Create a new product
+server.post('/products', function (req, res, next) {
+  console.log('POST /products params=>' + JSON.stringify(req.params));
+  console.log('POST /products body=>' + JSON.stringify(req.body));
+
+  // Validation of mandatory fields
+  if (req.body.name === undefined || req.body.price === undefined || req.body.quantity === undefined) {
+    return next(new errors.BadRequestError('name and price and quantity must be supplied'));
+  }
+
+  let newProduct = {
+    name: req.body.name, 
+    price: req.body.price,
+    quantity: req.body.quantity
+  };
+
+  // Create the product using the persistence engine
+  productsSave.create(newProduct, function (error, product) {
+    if (error) return next(new errors.InternalServerError(JSON.stringify(error.errors)));
+    res.send(201, product);
   });
 });
